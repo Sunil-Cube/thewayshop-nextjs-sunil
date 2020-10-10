c17939ac69a8b241cba5564399d7ed5c56e0b769
sunil-enprocon <sunil@enprocon.com>
2020-10-10 22:58:22 +0530

    prodctswise reducers

--- root-reducer.js
+++ root-reducer.js
@@ -2,12 +2,11 @@ import {combineReducers} from "redux";
 import { OurTeamReducer } from "./components/Layout/components/OurTeam/OurTeam.reducers";
 import ShopCategoriesReducer from "./components/Layout/components/ShopCategories/ShopCategories.reducer";
 import cartReducer from "./reducers/cart";
-
+import myProductReducer from "./reducers/myProductReducer";
 
 export const rootReducer = combineReducers({
     teamMembers : OurTeamReducer,
     shopCategories : ShopCategoriesReducer,
-    cart : cartReducer
+    cart : cartReducer,
+    products : myProductReducer
 })
-
-
